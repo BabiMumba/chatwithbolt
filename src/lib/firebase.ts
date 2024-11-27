a3c46e72c46f745093fb5dd7c039647872e50a9d
BabiMumba <babimumba243@gmail.com>
2024-11-27 02:30:09 +0200

    change

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhYe37GCrjl9nQvrEfnn-8OeN5OvSifNA",
-  authDomain: "mychatpp-63d89.firebaseapp.com",
-  databaseURL: "https://mychatpp-63d89-default-rtdb.firebaseio.com",
-  projectId: "mychatpp-63d89",
-  storageBucket: "mychatpp-63d89.firebasestorage.app",
-  messagingSenderId: "766601267330",
-  appId: "1:766601267330:web:a2036da376148b994e0c9b"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-database-url",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
 };
 
 const app = initializeApp(firebaseConfig);
